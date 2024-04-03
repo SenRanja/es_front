80f3e2c7fa126571eaf4844fd806e8042da4efd0
shenyanjian@gmail.com <shenyanjian@gmail.com>
2024-04-03 17:14:05 +0800

    first commit

---
+++ src/api/user.js
@@ -0,0 +1,42 @@
+import axios from '~/axios'
+
+export function getUserList(page, query = {}) {
+    let q = []
+    for (const key in query) {
+        if (query[key]) {
+            q.push(`${key}=${encodeURIComponent(query[key])}`)
+        }
+    }
+    let r = q.join("&")
+    // r = r ? ("?"+r) : ""
+
+    return axios.get(`/api/user/?format=json&page=${page}&${r}`)
+}
+// export function updateUserStatus(){
+//     return axios.put(`/api/user/?format=json`)
+// }
+export function createUser(data) {
+    return axios.post(`/api/user/`, data)
+}
+export function updateUser(id, data) {
+    return axios.put(`/api/user/${id}/`, data)
+}
+export function deleteUser(id) {
+    return axios.delete(`/api/user/${id}/`)
+}
+
+
+
+// 文件上传、导入
+export function uploadFile(file) {
+    
+
+    const formData = new FormData();
+    formData.append('excel_file', file);
+
+    return axios.post('/api/import_user_from_excel/', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+}
