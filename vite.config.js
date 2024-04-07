245f7b92590621b1ad7883ba8c09fba8fb5bf065
shenyanjian@gmail.com <shenyanjian@gmail.com>
2024-04-07 11:15:51 +0800

    题库难易改中文

--- vite.config.js
+++ vite.config.js
@@ -16,16 +16,16 @@ export default defineConfig({
         // 配置监听的ip和端口
         host: '0.0.0.0',
         port: 80,
-        // proxy: {
-        // // 这里 /api 就代理 真实的baseURL， 该配置在 axios.js 应用
-        // // 配置此 proxy 可以直接解决跨域问题，但是原理利用了前端服务器作为所有请求的代理，性能上看，前端服务器作为了中间人
-        //   '/api': {
-        //     // target: 'http://127.0.0.1:8000/',
-        //     target: "http://es_backend:8000/",
-        //     changeOrigin: true,
-        //     rewrite: (path) => path.replace(/^\/api/, ''),
-        //   },
-        // }
+        proxy: {
+        // 这里 /api 就代理 真实的baseURL， 该配置在 axios.js 应用
+        // 配置此 proxy 可以直接解决跨域问题，但是原理利用了前端服务器作为所有请求的代理，性能上看，前端服务器作为了中间人
+          '/api': {
+            // target: 'http://127.0.0.1:8000/',
+            target: "http://192.227.167.113:8000",
+            changeOrigin: true,
+            // rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        }
     },
 
     plugins: [vue(), WindiCSS()],
