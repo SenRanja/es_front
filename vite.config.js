3f4c6193e9571041919a8d1b41d6c4c513a78c80
shenyanjian@gmail.com <shenyanjian@gmail.com>
2024-04-22 16:07:55 +0800

    shenyanjian's change

--- vite.config.js
+++ vite.config.js
@@ -18,15 +18,15 @@ export default defineConfig({
         // 配置监听的ip和端口
         host: '0.0.0.0',
         port: 80,
-        // proxy: {
-        // // 这里 /api 就代理 真实的baseURL， 该配置在 axios.js 应用
-        // // 配置此 proxy 可以直接解决跨域问题，但是原理利用了前端服务器作为所有请求的代理，性能上看，前端服务器作为了中间人
-        //   '/api': {
-        //     target: "http://1.92.65.117:80",
-        //     changeOrigin: true,
-        //     // rewrite: (path) => path.replace(/^\/api/, ''),
-        //   },
-        // }
+        proxy: {
+        // 这里 /api 就代理 真实的baseURL， 该配置在 axios.js 应用
+        // 配置此 proxy 可以直接解决跨域问题，但是原理利用了前端服务器作为所有请求的代理，性能上看，前端服务器作为了中间人
+          '/api': {
+            target: "http://1.92.65.117:80",
+            changeOrigin: true,
+            // rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        }
     },
     css: {
         preprocessorOptions: {
